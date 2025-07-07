4f083ee5cca810a9df7bb50a22e71ead459f18cc
raonyvf <raonyvf@gmail.com>
2025-07-07 15:43:11 +0200

    Add basic login controller and routes

--- routes/auth.php
+++ routes/auth.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-// Placeholder for auth routes (Breeze would generate real routes)
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+    ->name('login');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->name('logout');
