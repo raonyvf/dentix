06b5116b9755de8687de179e312327744a7747c8
raonyvf <raonyvf@gmail.com>
2025-07-07 21:58:11 +0200

    Redirect root to login or admin

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::check()) {
+        return redirect()->route('admin.index');
+    }
+
+    return redirect()->route('login');
 });
 
 require __DIR__.'/auth.php';
