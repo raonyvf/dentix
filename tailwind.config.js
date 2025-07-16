edfa8fea372a0cbf6fe8568693082ef6296b3f9e
raonyvf <raonyvf@gmail.com>
2025-07-16 10:06:56 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,8 @@ export default {
     theme: {
         extend: {
             colors: {
-                body: '#64748B',
-                dark: '#212B36',
                 primary: '#3C50E0',
-                secondary: '#80CAEE',
-                success: '#3CD856',
-                warning: '#FBBF24',
-                danger: '#F75555',
-                info: '#0EA5E9',
                 stroke: '#E2E8F0',
-                gray: '#F4F7FF',
                 'gray-2': '#F5F7FA',
             },
         },
