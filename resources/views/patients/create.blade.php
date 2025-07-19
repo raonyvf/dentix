8f3a989e307d021c69610a48d9c036cc0993e7d6
raonyvf <raonyvf@gmail.com>
2025-07-19 17:49:14 +0200

    Reorder patient address fields

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -78,7 +78,15 @@
                     </div>
                     <div class="sm:col-span-2">
                         <label class="mb-2 block text-sm font-medium text-gray-700">Rua</label>
-                        <input type="text" name="endereco_rua" value="{{ old('endereco_rua') }}" class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none" placeholder="Número e complemento" />
+                        <input type="text" name="endereco_rua" value="{{ old('endereco_rua') }}" class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none" />
+                    </div>
+                    <div>
+                        <label class="mb-2 block text-sm font-medium text-gray-700">Número</label>
+                        <input type="text" name="numero" value="{{ old('numero') }}" class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none" />
+                    </div>
+                    <div>
+                        <label class="mb-2 block text-sm font-medium text-gray-700">Complemento</label>
+                        <input type="text" name="complemento" value="{{ old('complemento') }}" class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none" />
                     </div>
                     <div>
                         <label class="mb-2 block text-sm font-medium text-gray-700">Bairro</label>
