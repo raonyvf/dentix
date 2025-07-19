ce61993e102e2e70d4582ee99422ae79495393a3
raonyvf <raonyvf@gmail.com>
2025-07-19 19:23:57 +0200

    feat: compute age from birth date

--- resources/views/patients/edit.blade.php
+++ resources/views/patients/edit.blade.php
@@ -49,8 +49,8 @@
                         <input class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none" type="text" name="ultimo_nome" value="{{ old('ultimo_nome', $ultimoNome) }}" required />
                     </div>
                     <div>
-                        <label class="mb-2 block text-sm font-medium text-gray-700">Idade</label>
-                        <input class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none" type="number" name="idade" value="{{ old('idade', $paciente->idade) }}" />
+                        <label class="mb-2 block text-sm font-medium text-gray-700">Data de nascimento</label>
+                        <input class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none" type="date" name="data_nascimento" value="{{ old('data_nascimento', optional($paciente->data_nascimento)->format('Y-m-d')) }}" />
                     </div>
                     <div>
                         <label class="mb-2 block text-sm font-medium text-gray-700">Telefone</label>
