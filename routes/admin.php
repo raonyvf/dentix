13bccfd8076353e5c0baa677fd818d71139a7d44
raonyvf <raonyvf@gmail.com>
2025-07-23 23:21:45 +0200

    Remove módulo de profissionais

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Admin\ClinicController;
 use App\Http\Controllers\Admin\CadeiraController;
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\Admin\ProfessionalController;
 use App\Http\Controllers\Admin\FormularioController;
 use App\Http\Controllers\Admin\PatientController;
 use App\Http\Controllers\Admin\AgendaController;
@@ -24,8 +23,6 @@ Route::resource('clinicas', ClinicController::class)
 Route::resource('cadeiras', CadeiraController::class);
 Route::resource('perfis', ProfileController::class)->parameters(['perfis' => 'perfil']);
 Route::resource('usuarios', UserController::class)->only(['index','edit','update']);
-Route::resource('profissionais', ProfessionalController::class)
-    ->parameters(['profissionais' => 'profissional']);
 
 Route::resource('formularios', FormularioController::class);
 Route::resource('pacientes', PatientController::class)
