4b29de78ad0b3ceb6a128192d92648446fd1ce7f
raonyvf <raonyvf@gmail.com>
2025-07-23 16:41:44 +0200

    Add gender select to professional create form

--- resources/views/admin/professionals/create.blade.php
+++ resources/views/admin/professionals/create.blade.php
@@ -50,6 +50,14 @@
                     <input class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none" type="date" name="data_nascimento" value="{{ old('data_nascimento') }}" />
                 </div>
                 <div>
+                    <label class="mb-2 block text-sm font-medium text-gray-700">Sexo</label>
+                    <select name="sexo" class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none">
+                        <option value="">Selecionar sexo</option>
+                        <option value="M">Masculino</option>
+                        <option value="F">Feminino</option>
+                    </select>
+                </div>
+                <div>
                     <label class="mb-2 block text-sm font-medium text-gray-700">CPF</label>
                     <input class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none" type="text" name="cpf" value="{{ old('cpf') }}" />
                 </div>
