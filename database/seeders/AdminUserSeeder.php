596ae91e74ed67aac93a344cc110e304241e68f4
raonyvf <raonyvf@gmail.com>
2025-07-24 10:55:20 +0200

    fix seeder for new person relation

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -7,16 +7,29 @@ use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Models\Profile;
 use App\Models\Permission;
+use App\Models\Person;
 
 class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
+        $person = Person::firstOrCreate(
+            [
+                'email' => 'admin@example.com',
+                'organization_id' => 1,
+            ],
+            [
+                'first_name' => 'Admin',
+                'last_name' => 'User',
+            ]
+        );
+
         $user = User::updateOrCreate(
             ['email' => 'admin@example.com'],
             [
-                'name' => 'Admin',
-                'password' => Hash::make('password')
+                'password' => Hash::make('password'),
+                'organization_id' => $person->organization_id,
+                'person_id' => $person->id,
             ]
         );
 
