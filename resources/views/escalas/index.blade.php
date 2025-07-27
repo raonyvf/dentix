a92f53503a4270a2315382a36b3ce3d23037c3a4
raonyvf <raonyvf@gmail.com>
2025-07-27 14:44:06 +0200

    fix: adjust escala view layout

--- resources/views/escalas/index.blade.php
+++ resources/views/escalas/index.blade.php
@@ -33,24 +33,22 @@
     </div>
 </form>
 <div class="overflow-x-auto bg-white rounded shadow">
-    <table class="min-w-full text-sm">
+    <table class="min-w-full table-fixed text-sm border-collapse">
         <thead>
             <tr>
-                <th class="p-2 bg-gray-50 w-24 text-left sticky z-10"
-                    x-bind:style="sidebarCollapsed ? 'left:5rem' : 'left:16rem'">Cadeira</th>
+                <th class="p-2 bg-gray-50 w-24 text-left sticky left-64 z-10">Cadeira</th>
                 @foreach($dias as $d)
-                    <th class="p-2 bg-gray-50 text-left capitalize min-w-[576px]">{{ ucfirst($d) }}</th>
+                    <th class="p-2 bg-gray-50 text-left capitalize border" style="width:14.28%">{{ ucfirst($d) }}</th>
                 @endforeach
             </tr>
         </thead>
         <tbody>
             @foreach($cadeiras as $cadeira)
                 <tr class="border-t">
-                    <td class="bg-gray-50 w-24 p-2 whitespace-nowrap sticky"
-                        x-bind:style="sidebarCollapsed ? 'left:5rem' : 'left:16rem'">{{ $cadeira->nome }}</td>
+                    <td class="bg-gray-50 w-24 p-2 whitespace-nowrap sticky left-64">{{ $cadeira->nome }}</td>
                     @foreach($dias as $d)
                         @php $items = $escalas[$cadeira->id][$d] ?? collect(); @endphp
-                        <td class="min-w-[576px] align-top p-2">
+                        <td class="p-2 border bg-gray-50 align-top" style="width:14.28%">
                             @if($items->isNotEmpty())
                                 @php
                                     $sorted = $items->sortBy('hora_inicio')->values();
@@ -85,7 +83,7 @@
                                         @if(optional($it->profissional->user)->especialidade)
                                             <div class="text-xs text-gray-500">{{ optional($it->profissional->user)->especialidade }}</div>
                                         @endif
-                                        <div class="relative h-3 bg-emerald-100 rounded mt-1 overflow-hidden">
+                                        <div class="relative h-2 bg-emerald-100 rounded mt-1 overflow-hidden">
                                             <div class="absolute inset-0 pointer-events-none" style="background-image:repeating-linear-gradient(to right,#d1d5db 0,#d1d5db 1px,transparent 1px,transparent calc(100%/48));"></div>
                                             <div class="absolute top-0 h-full bg-emerald-400 rounded" style="left:{{ $left }}%; width:{{ $width }}%;"></div>
                                         </div>
