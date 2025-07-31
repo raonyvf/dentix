5e697aa3d908467c14fd9126e6b5bcff5fb9e7c1
raonyvf <raonyvf@gmail.com>
2025-07-31 20:48:36 +0200

    Add monthly view for work schedules

--- app/Http/Controllers/Admin/EscalaTrabalhoController.php
+++ app/Http/Controllers/Admin/EscalaTrabalhoController.php
@@ -16,17 +16,10 @@ class EscalaTrabalhoController extends Controller
         $user = auth()->user();
         $clinics = $user->isOrganizationAdmin() ? Clinic::all() : $user->clinics()->get();
         $clinicId = $request->input('clinic_id', $clinics->first()->id ?? null);
-        $week = $request->input('week');
-        $week = $week ? Carbon::parse($week)->startOfWeek(Carbon::MONDAY) : Carbon::now()->startOfWeek(Carbon::MONDAY);
+        $view = $request->input('view', 'week');
 
-        $semanasDisponiveis = collect(range(-2, 5))->map(fn($i) => Carbon::now()->startOfWeek(Carbon::MONDAY)->addWeeks($i));
         $dias = ['segunda','terca','quarta','quinta','sexta','sabado','domingo'];
         $cadeiras = $clinicId ? Cadeira::where('clinic_id', $clinicId)->get() : collect();
-        $escalas = EscalaTrabalho::with(['profissional.person','profissional.user'])
-            ->where('clinic_id', $clinicId)
-            ->where('semana', $week->toDateString())
-            ->get()
-            ->groupBy(['cadeira_id','dia_semana']);
         $dentistas = Profissional::where(function($q){
                 $q->where('funcao', 'Dentista')->orWhere('cargo', 'Dentista');
             })
@@ -36,7 +29,38 @@ class EscalaTrabalhoController extends Controller
             ->with(['person'])
             ->get();
 
-        return view('escalas.index', compact('clinics','clinicId','week','dias','cadeiras','escalas','dentistas','semanasDisponiveis'));
+        if ($view === 'month') {
+            $month = $request->input('month');
+            $month = $month ? Carbon::parse($month)->startOfMonth() : Carbon::now()->startOfMonth();
+            $mesesDisponiveis = collect(range(-2, 2))->map(fn($i) => Carbon::now()->startOfMonth()->addMonths($i));
+
+            $weeks = collect();
+            $current = $month->copy()->startOfWeek(Carbon::MONDAY);
+            $last = $month->copy()->endOfMonth()->endOfWeek(Carbon::SUNDAY);
+            while ($current->lte($last)) {
+                $weeks->push($current->copy());
+                $current->addWeek();
+            }
+
+            $escalas = EscalaTrabalho::with(['profissional.person','profissional.user'])
+                ->where('clinic_id', $clinicId)
+                ->whereBetween('semana', [$weeks->first()->toDateString(), $weeks->last()->toDateString()])
+                ->get()
+                ->groupBy(['semana','cadeira_id','dia_semana']);
+
+            return view('escalas.index', compact('clinics','clinicId','view','month','weeks','dias','cadeiras','escalas','dentistas','mesesDisponiveis'));
+        }
+
+        $week = $request->input('week');
+        $week = $week ? Carbon::parse($week)->startOfWeek(Carbon::MONDAY) : Carbon::now()->startOfWeek(Carbon::MONDAY);
+        $semanasDisponiveis = collect(range(-2, 5))->map(fn($i) => Carbon::now()->startOfWeek(Carbon::MONDAY)->addWeeks($i));
+        $escalas = EscalaTrabalho::with(['profissional.person','profissional.user'])
+            ->where('clinic_id', $clinicId)
+            ->where('semana', $week->toDateString())
+            ->get()
+            ->groupBy(['cadeira_id','dia_semana']);
+
+        return view('escalas.index', compact('clinics','clinicId','view','week','dias','cadeiras','escalas','dentistas','semanasDisponiveis'));
     }
 
     public function store(Request $request)
@@ -85,7 +109,15 @@ class EscalaTrabalhoController extends Controller
             ]);
         }
 
-        return redirect()->route('escalas.index', ['clinic_id' => $data['clinic_id'], 'week' => $data['semana']])
+        $params = ['clinic_id' => $data['clinic_id'], 'week' => $data['semana']];
+        if ($request->filled('view')) {
+            $params['view'] = $request->input('view');
+        }
+        if ($request->filled('month')) {
+            $params['month'] = $request->input('month');
+        }
+
+        return redirect()->route('escalas.index', $params)
             ->with('success', 'Escala salva com sucesso.');
     }
 }
