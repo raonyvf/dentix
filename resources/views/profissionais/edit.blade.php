1fe862bbd050b75c93a466cf2ab8d9c43c419b41
raonyvf <raonyvf@gmail.com>
2025-08-01 16:59:56 +0200

    Make CRO UF required for dentists

--- resources/views/profissionais/edit.blade.php
+++ resources/views/profissionais/edit.blade.php
@@ -220,8 +220,8 @@
                     <input type="number" name="cro" value="{{ old('cro', $profissional->cro ?? '') }}" x-bind:required="funcao === 'Dentista'" class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none" />
                 </div>
                 <div>
-                    <label class="text-sm font-medium text-gray-700 mb-2 block">UF do CRO</label>
-                    <select name="cro_uf" class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none">
+                    <label class="text-sm font-medium text-gray-700 mb-2 block">UF do CRO <span x-show="funcao === 'Dentista'" class="text-red-500">*</span></label>
+                    <select name="cro_uf" x-bind:required="funcao === 'Dentista'" class="w-full rounded border-[1.5px] border-stroke bg-gray-2 py-3 px-5 text-sm text-black focus:border-primary focus:outline-none">
                         <option value="">Selecione</option>
                         @foreach(['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'] as $uf)
                             <option value="{{ $uf }}" @selected(old('cro_uf', $profissional->cro_uf ?? '') === $uf)>{{ $uf }}</option>
