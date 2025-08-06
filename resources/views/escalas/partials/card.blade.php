915eacc4a3f479b6f5bc5f577e0ca7238d181e65
raonyvf <raonyvf@gmail.com>
2025-08-06 16:48:58 +0200

    feat: highlight days outside month

--- resources/views/escalas/partials/card.blade.php
+++ resources/views/escalas/partials/card.blade.php
@@ -1,5 +1,6 @@
 @php
     $diaSemana = $diaSemana ?? null;
+    $out = $out ?? false;
     $horarioClinica = $clinic?->horarios->firstWhere('dia_semana', $diaSemana);
 
     $inicio = \Carbon\Carbon::parse($it->hora_inicio);
@@ -18,7 +19,7 @@
     $duracao = $inicio->diffInMinutes($fim);
     $percentual = $totalMinutos > 0 ? ($duracao / $totalMinutos) * 100 : 0;
 @endphp
-<div class="mb-2 p-2 rounded bg-emerald-50 text-sm escala-card relative cursor-pointer" data-id="{{ $it->id }}" data-profissional="{{ $it->profissional_id }}" data-hora-inicio="{{ $it->hora_inicio }}" data-hora-fim="{{ $it->hora_fim }}" data-cadeira="{{ $it->cadeira_id }}" data-date="{{ \Carbon\Carbon::parse($it->semana)->addDays($it->dia_semana - 1)->format('Y-m-d') }}">
+<div class="mb-2 p-2 rounded bg-emerald-50 text-sm escala-card relative cursor-pointer {{ $out ? 'opacity-50' : '' }}" data-id="{{ $it->id }}" data-profissional="{{ $it->profissional_id }}" data-hora-inicio="{{ $it->hora_inicio }}" data-hora-fim="{{ $it->hora_fim }}" data-cadeira="{{ $it->cadeira_id }}" data-date="{{ \Carbon\Carbon::parse($it->semana)->addDays($it->dia_semana - 1)->format('Y-m-d') }}">
     <div class="font-semibold whitespace-nowrap overflow-hidden text-ellipsis">
         {{ optional($it->profissional->pessoa)->primeiro_nome }} {{ optional($it->profissional->pessoa)->ultimo_nome }}
     </div>
