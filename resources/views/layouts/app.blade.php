43cb2455173b070e07e079dc06b7bb5d33b45383
raonyvf <raonyvf@gmail.com>
2025-08-10 18:14:44 +0200

    Include navigation and top bar in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,14 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="antialiased">
+    @unless($hideNav ?? false)
+        @include('partials.nav')
+    @endunless
+
+    @unless($hideErrors ?? false)
+        @include('partials.topbar')
+    @endunless
+
     @yield('content')
 </body>
 </html>
