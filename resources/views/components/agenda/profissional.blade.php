ec5b3105622e5641e8863f860e46de292315d240
raonyvf <raonyvf@gmail.com>
2025-08-10 15:59:09 +0200

    Delete resources directory

--- resources/views/components/agenda/profissional.blade.php
+++
@@ -1,6 +0,0 @@
-@props(['name', 'active' => false])
-@php
-    $classes = 'px-4 py-2 rounded border text-sm whitespace-nowrap';
-    $classes .= $active ? ' bg-primary text-white' : ' bg-white text-gray-700';
-@endphp
-<button {{ $attributes->merge(['class' => $classes]) }}>{{ $name }}</button>
