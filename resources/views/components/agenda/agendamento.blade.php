54b7af73167fa86fcadfc4f2568e74cff8858e73
raonyvf <raonyvf@gmail.com>
2025-08-11 14:45:17 +0200

    ajusta estilos do card de agendamento

--- resources/views/components/agenda/agendamento.blade.php
+++ resources/views/components/agenda/agendamento.blade.php
@@ -12,15 +12,21 @@
         'cancelado' => 'Cancelado',
         default => 'Sem confirmação',
     };
+    $borderColor = match ($status) {
+        'confirmado' => 'rgb(52 211 153 / var(--tw-bg-opacity, 1))',
+        'pendente' => 'rgb(250 204 21 / var(--tw-bg-opacity, 1))',
+        'cancelado' => 'rgb(248 113 113 / var(--tw-bg-opacity, 1))',
+        default => 'rgb(156 163 175 / var(--tw-bg-opacity, 1))',
+    };
     $color = match ($status) {
-        'confirmado' => 'bg-green-100 text-green-700 border-green-800',
-        'pendente' => 'bg-yellow-100 text-yellow-700 border-yellow-800',
-        'cancelado' => 'bg-red-100 text-red-700 border-red-800',
-        default => 'bg-gray-100 text-gray-700 border-gray-800',
+        'confirmado' => 'bg-green-100 text-green-700',
+        'pendente' => 'bg-yellow-100 text-yellow-700',
+        'cancelado' => 'bg-red-100 text-red-700',
+        default => 'bg-gray-100 text-gray-700',
     };
 @endphp
 {{-- Additional data-* attributes are forwarded to the root div --}}
-<div {{ $attributes->merge(['class' => "rounded p-2 text-xs border $color"]) }}>
+<div {{ $attributes->merge(['class' => "rounded p-2 text-xs border-2 m-0.5 $color", 'style' => "border-color: $borderColor"]) }}>
     <div class="font-bold text-sm">{{ $paciente }}</div>
     @if($inicio && $fim)
         <div>{{ $inicio }} - {{ $fim }}</div>
