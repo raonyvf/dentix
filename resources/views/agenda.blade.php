1d3c7ebe466a87c455199ee839cba1ee85c50cc9
raonyvf <raonyvf@gmail.com>
2025-08-20 19:56:09 +0200

    Replace waitlist month icon with inline SVG

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -122,7 +122,9 @@
                 <h2 class="font-semibold">Lista de Espera</h2>
                 <div class="flex items-center gap-2">
                     <button id="btn-waitlist-month" class="p-1 text-blue-600 hover:text-blue-800">
-                        <i class="fa fa-calendar"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                        </svg>
                     </button>
                     <button id="waitlist-add" class="p-1 text-blue-600 hover:text-blue-800">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
